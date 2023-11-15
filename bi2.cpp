95aa1083c12357e41cf85812966a4447cb2ec743
Rickjoosten26 <rick.joosten@hotmail.com>
2023-11-15 13:51:02 +0100

    ja

--- bi2.cpp
+++ bi2.cpp
@@ -54,27 +54,32 @@ bool fits(Box a, Box b){
 int size = 0;
 
 
-bool matching(vector<vector<bool>>& graph, int u, vector<bool>& seen, vector<int>& matchR){
+
+bool matching(vector<vector<bool>>& graph, int u, vector<bool>& seen, vector<int>& matchR, vector<vector<int>>& dp){
 	for(int v = 0; v < size; v++){
 		if(graph.at(u).at(v) && !seen.at(v)){
 			seen.at(v) = true;
-			if(matchR.at(v) < 0 || matching(graph, matchR.at(v), seen, matchR)){
+			if(matchR.at(v) < 0 || (dp.at(matchR.at(v)).at(u) != -1 && matching(graph, matchR.at(v), seen, matchR, dp))){
 				matchR.at(v) = u;
+                if(matchR.at(v) != -1){
+                    dp.at(u).at(matchR.at(u)) = 1;
+                }
 				return true;
 			}
 		}
 	}
+    if(matchR.at(u) != -1){
+        dp.at(u).at(matchR.at(u)) = 0;
+    }
 	return false;
 }
 
-int max_matching(vector<vector<bool>>& graph){
-	vector<int> matchR(size, -1);
-
+int max_matching(vector<vector<bool>>& graph, vector<int>& matchR, vector<vector<int>>& dp){
 	int result = 0;	
 
 	for(int u = 0; u < size; u++){
 		vector<bool> seen(size, false);
-		if(matching(graph, u, seen, matchR)){
+		if(matching(graph, u, seen, matchR, dp)){
 			result++;
 		}
 	}
@@ -88,6 +93,9 @@ int main(){
     cin >> temp;
 	size = temp;
 
+    vector<int> matchR(size, -1);
+    vector<vector<int>> dp(size, vector<int>(size, -1));
+    
     vector<Box> original_boxes;
 
     for (int i = 0; i < size; i++){
@@ -105,7 +113,7 @@ int main(){
 			graph.at(i).at(j) = canStore;
 		}
 	}
-	int result = size - max_matching(graph);
+	int result = size - max_matching(graph, matchR, dp);
 	cout << result << endl;
 	cout << double( clock() - startTime ) / (double)CLOCKS_PER_SEC<< " seconds." << endl;
 	return 0;
